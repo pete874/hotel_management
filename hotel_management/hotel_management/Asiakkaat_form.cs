839f186c8978f33683121572b44954a7fdfe4fd4
SaarinenVille <ville.saarinen@edu.keuda.fi>
2023-03-08 15:23:21 +0200

    käyttäjätunnuksen ja salasanan generointi

--- hotel_management/hotel_management/Asiakkaat_form.cs
+++ hotel_management/hotel_management/Asiakkaat_form.cs
@@ -24,6 +24,24 @@ namespace hotel_management
             AsiakkaatDG.DataSource = asiakkaat.HaeAsiakkaat();
         }
 
+        public String salasana()
+        {
+            char[] chars = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
+                             'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
+                             'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 
+                             'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 
+                             'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 
+                             'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+            String salis = "";
+            Random random = new Random();
+
+            for(int i = 0; i < 10; i++) 
+            {
+                int randomInt = random.Next(0, 61);
+                salis += chars[randomInt];
+            }
+            return salis;
+        }
         private void LisaaBT_Click(object sender, EventArgs e)
         {
             String etunimi = EtunimiTB.Text;
@@ -31,8 +49,8 @@ namespace hotel_management
             String lahiosoite = LahiosoiteTB.Text;
             String postinumero = PostinumeroTB.Text;
             String ptoimipaikka = PToimipaikkaTB.Text;
-            String kayttajanimi = KayttajatunnusTB.Text;
-            String salasana = SalasanaTB.Text;
+            String kayttajanimi = etunimi.Substring(0, 3).ToLower() + sukunimi.Substring(0, 5).ToLower();
+            String salsana = salasana();
 
             if (etunimi.Trim().Equals("") || 
                 sukunimi.Trim().Equals("") || 
@@ -40,13 +58,13 @@ namespace hotel_management
                 postinumero.Trim().Equals("") || 
                 ptoimipaikka.Trim().Equals("") || 
                 kayttajanimi.Trim().Equals("") || 
-                salasana.Trim().Equals(""))
+                salsana.Trim().Equals(""))
             {
                 MessageBox.Show("Täytä kaikki kentät", "Virhe syötössä", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             else
             {
-                Boolean lisaaAsiakas = asiakkaat.LisaaAsiakas(etunimi, sukunimi, lahiosoite, postinumero, ptoimipaikka, kayttajanimi, salasana);
+                Boolean lisaaAsiakas = asiakkaat.LisaaAsiakas(etunimi, sukunimi, lahiosoite, postinumero, ptoimipaikka, kayttajanimi, salsana);
                 if (lisaaAsiakas)
                 {
                     MessageBox.Show("Uusi asiakas lisätty!", "Asiakkaan lisäys", MessageBoxButtons.OK, MessageBoxIcon.Information);
