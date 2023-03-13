ad7c20b01320f5c843cb8d690a22dac9ad1aa37a
Petri Nurmi <pete874@hotmail.com>
2023-03-13 12:37:27 +0200

    Lisää ajanvaraus tarkistus

--- hotel_management/hotel_management/VARAUS.cs
+++ hotel_management/hotel_management/VARAUS.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace hotel_management
 {
@@ -33,20 +34,24 @@ namespace hotel_management
         {
             //tehdään uusi MySqlCommand.
             MySqlCommand komento = new MySqlCommand();
-            //Tallennetaan stringiin SQL kysely. Tämä voidaan myös laittaa suoraan MySqlCommandin sulkeiden sisään yhteys-funktion kanssa, niikuin ylimmässä funktiossa on tehty.
-            String lisaysKysely = "INSERT INTO varaukset (tyontekija, huonetyyppi, huonenro, sisaan, ulos) VALUES (@tyo, @hty, @hnu, @sis, @ulo); ";
 
+            //Tallennetaan stringiin SQL kysely. Tämä voidaan myös laittaa suoraan MySqlCommandin sulkeiden sisään yhteys-funktion kanssa, niikuin ylimmässä funktiossa on tehty.
+            //String lisaysKysely2 = "INSERT INTO varaukset (tyontekija, huonetyyppi, huonenro, sisaan, ulos) VALUES (@tyo, @hty, @hnu, @sis, @ulo); ";
+            
+            //Tämä komento tarkistaa, onko huone jo varattu kyseisenä ajankohtana
+            String lisaysKysely = "SELECT COUNT(*) FROM varaukset WHERE huonenro = @hnu AND (sisaan <= @sis AND ulos >= @ulo);";
 
             //Asettaa Stringin SQL komennoksi komento muuttujaan
             komento.CommandText = lisaysKysely;
+      
 
             //Asettaa otaYhteys funktion SQL yhteydeksi Connection komennolla.  Tämä voidaan myös laittaa yhteydeksi MySqlCommandin sisään pilkun jälkeen toiseksi parametriksi
             komento.Connection = yhteys.otaYhteys();
 
 
             //Tehdään C# puolelta luettavat muuttujat, joita voidaan käyttää SQL komennossa. Huom. Normaalit C# muuttujat eivät toimi SQL komennossa.
-            komento.Parameters.Add("@tyo", MySqlDbType.VarChar).Value = tyontekija;
-            komento.Parameters.Add("@hty", MySqlDbType.VarChar).Value = huonetyyppi;
+            //komento.Parameters.Add("@tyo", MySqlDbType.VarChar).Value = tyontekija;
+            //komento.Parameters.Add("@hty", MySqlDbType.VarChar).Value = huonetyyppi;
             komento.Parameters.Add("@hnu", MySqlDbType.Int32).Value = huonenro;
             komento.Parameters.Add("@sis", MySqlDbType.VarChar).Value = sisaan;
             komento.Parameters.Add("@ulo", MySqlDbType.VarChar).Value = ulos;
@@ -54,18 +59,26 @@ namespace hotel_management
 
             yhteys.avaaYhteys();
 
-            //ExecuteNonQuerya käytetään SQL komentoihin, joilla ei palauteta dataa vaan pelkästään tallennetaan dataa tietokantaan(UPDATE,INSERT,DELETE)
-            //Jos SQL-lisäyskysely onnistuu esimerkiksi päivittämään rivin tietokannnassa, palauttaa ExecuteNonQuery int-tyyppinä vaikutuksen alaisena olevien rivien määrän
-            // eli kuinka monta riviä esim päivitetty tai poistettu.
-            // Tässä kyseisessä komennossa rivejä palautuu aina yksi, jos on onnistuttu muuttamaan dataa. Jos yhtäkään riviä ei muutettu, palautuu numero -1
-            //Alhaalla tarkistetaan onko riviä onnistuttu muuttamaan ja palautetaan joko true tai false boolean.
-            if (komento.ExecuteNonQuery() == 1)
+            //tallennetaan lisayskyselyssä tulleiden rivien määrä
+            int count = Convert.ToInt32(komento.ExecuteScalar());
+
+            //jos rivit on 0, niin suoritetaan lisäys, muuten annetaan virhe.
+            if (count < 1)
             {
+                MySqlCommand komento2 = new MySqlCommand("INSERT INTO varaukset (tyontekija, huonetyyppi, huonenro, sisaan, ulos) VALUES (@tyo, @hty, @hnu, @sis, @ulo); ", yhteys.otaYhteys());
+                komento2.Parameters.Add("@tyo", MySqlDbType.VarChar).Value = tyontekija;
+                komento2.Parameters.Add("@hty", MySqlDbType.VarChar).Value = huonetyyppi;
+                komento2.Parameters.Add("@hnu", MySqlDbType.Int32).Value = huonenro;
+                komento2.Parameters.Add("@sis", MySqlDbType.Date).Value = sisaan;
+                komento2.Parameters.Add("@ulo", MySqlDbType.Date).Value = ulos;
+                yhteys.avaaYhteys();
+                komento2.ExecuteNonQuery();
                 yhteys.suljeYhteys();
                 return true;
             }
             else
             {
+                MessageBox.Show("Huone onjo varattu kyseisenä ajankohtana");
                 yhteys.suljeYhteys();
                 return false;
             }
